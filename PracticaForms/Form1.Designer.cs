d61c396a9689d179c7c3d18809c889e546c78e46
Celeste <cecuercu@gmail.com>
2021-02-18 18:08:11 +0100

    Finished

--- PracticaForms/Form1.Designer.cs
+++ PracticaForms/Form1.Designer.cs
@@ -34,16 +34,18 @@ namespace PracticaForms
             // 
             // userControl11
             // 
-            this.userControl11.Location = new System.Drawing.Point(124, 12);
+            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.userControl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userControl11.Location = new System.Drawing.Point(54, 33);
             this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(554, 344);
+            this.userControl11.Size = new System.Drawing.Size(554, 278);
             this.userControl11.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(665, 356);
             this.Controls.Add(this.userControl11);
             this.Name = "Form1";
             this.Text = "Form1";
